0e9ac4cf775991968c82f826db09c1749590d275
Jairo <jotafer19@users.noreply.github.com>
2025-07-07 12:33:25 +0200

    Fix user route

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -4,7 +4,6 @@ const usersController = require("../controllers/usersController")
 const usersRouter = Router()
 
 usersRouter.post("/signup", usersController.createUser)
-// usersRouter.post("/login")
-// usersRouter.post("/admin")
+
 
 module.exports = usersRouter;
